2bc14686713b7ea97ad833c939df7d5f5ff8d275
Naoki Yaguchi <murooka.1129.x50@gmail.com>
2019-07-01 02:57:11 +0900

    Tweak

--- jersey-java11/src/main/java/jersey/java11/resource/RootResource.java
+++ jersey-java11/src/main/java/jersey/java11/resource/RootResource.java
@@ -1,16 +1,18 @@
 package jersey.java11.resource;
 
+import lombok.extern.java.Log;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+@Log
 @Path("/")
 public class RootResource {
   @GET
-  @Path("/")
   @Produces(MediaType.TEXT_PLAIN)
   public String getRoot() {
+    log.info("Hello, world!");
     return "Hello, world!";
   }
 
